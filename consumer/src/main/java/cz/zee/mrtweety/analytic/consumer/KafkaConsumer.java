ecf8d07946606b71d9e274f6fe09ec0dbf85dd98
Jakub Horak <kubahorak@gmail.com>
2016-03-28 18:35:17 +0200

    Add producer storing tweets to Kafka.

---
+++ consumer/src/main/java/cz/zee/mrtweety/analytic/consumer/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package cz.zee.mrtweety.analytic.consumer;
+
+/**
+ * Consumes Kafka messages.
+ * @author Jakub Horak
+ */
+public class KafkaConsumer {
+
+    public static void main(String[] args) {
+        System.out.println("TODO consuming");
+    }
+
+}
