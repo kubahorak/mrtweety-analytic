a37f5bc36b8a1d4d5fe94a9cbd627e6d949b150c
Jakub Horak <kubahorak@gmail.com>
2016-10-15 13:11:06 +0200

    Replace Kafka and HBC with dstream-twitter library. Write top 5 hashtags to a file.

---
+++ spark/src/main/java/cz/zee/mrtweety/analytic/spark/Application.java
@@ -0,0 +1,111 @@
+package cz.zee.mrtweety.analytic.spark;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.Minutes;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.twitter.TwitterUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import scala.Tuple2;
+import twitter4j.Status;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+/**
+ * Spark main application class.
+ * @author Jakub Horak
+ */
+public class Application implements Serializable {
+
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) {
+        new Application().start();
+    }
+
+    private void start() {
+        SparkConf conf = new SparkConf().setAppName("MrTweety Analytic").setMaster("local[*]");
+
+        Properties twitterProperties = load("/twitter.properties");
+
+        // setup processing with batch interval
+        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.seconds(10));
+
+        // set the connection system properties for Twitter4J
+        System.setProperty("twitter4j.oauth.consumerKey", twitterProperties.getProperty("consumerKey"));
+        System.setProperty("twitter4j.oauth.consumerSecret", twitterProperties.getProperty("consumerSecret"));
+        System.setProperty("twitter4j.oauth.accessToken", twitterProperties.getProperty("accessToken"));
+        System.setProperty("twitter4j.oauth.accessTokenSecret", twitterProperties.getProperty("accessTokenSecret"));
+
+        JavaReceiverInputDStream<Status> statuses = TwitterUtils.createStream(streamingContext);
+
+        JavaDStream<String> tweets = statuses.map(Status::getText);
+
+        JavaDStream<String> words = tweets.flatMap(x -> Arrays.asList(SPACE.split(x)))
+                .filter(word -> word.startsWith("#"));
+
+        JavaPairDStream<Integer, String> hashtagCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKeyAndWindow((i1, i2) -> i1 + i2, Minutes.apply(5))
+                .mapToPair(s -> new Tuple2<>(s._2(), s._1()))
+                .transformToPair(v1 -> v1.sortByKey(false));
+
+        hashtagCounts.foreach(pair -> {
+            List<Tuple2<Integer, String>> topHashtags = pair.take(5);
+            save(topHashtags);
+            return null;
+        });
+
+        streamingContext.start();
+        streamingContext.awaitTermination();
+    }
+
+    /**
+     * Saves a list of top hash tags as a JSON into a file.
+     * @param topHashtags list of tuples where key is count and value is the hashtag
+     */
+    private void save(List<Tuple2<Integer, String>> topHashtags) {
+
+        File file = new File("out/analytic.json");
+
+        JSONArray rootArray = new JSONArray();
+        topHashtags.forEach(tuple -> {
+            JSONObject stat = new JSONObject();
+            stat.put("hashtag", tuple._2());
+            stat.put("count", tuple._1());
+
+            rootArray.put(stat);
+        });
+
+        try {
+            FileUtils.write(file, rootArray.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Loads properties from the specified resource.
+     * @param resourceName name of the resource
+     * @return populated properties
+     */
+    private Properties load(String resourceName) {
+        Properties properties = new Properties();
+        try {
+            properties.load(getClass().getResourceAsStream(resourceName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+}
