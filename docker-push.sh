8f32eb44a58d57a37b5ddc203386401f26eff190
Jakub Horak <kubahorak@gmail.com>
2016-10-16 12:02:51 +0200

    Fix Travis.

--- docker-push.sh
+++ docker-push.sh
@@ -14,7 +14,8 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     export PATH=$PATH:$HOME/.local/bin
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
     
-    # Tag and push
+    # Build and push
+    docker build -t $IMAGE_NAME .
     echo "Pushing $IMAGE_NAME:latest"
     docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
     docker push "$REMOTE_IMAGE_URL:latest"
